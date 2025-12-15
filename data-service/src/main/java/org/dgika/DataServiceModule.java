249ee3f1dfae95a1524178db22cf2b5f107237cc
Ika <rexar.ika@gmail.com>
2025-12-15 17:58:49 +0300

    data-service: added liquibase migrations, created users, tickers, prices tables

---
+++ data-service/src/main/java/org/dgika/DataServiceModule.java
@@ -0,0 +1,11 @@
+package org.dgika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataServiceModule {
+    public static void main(String[] args) {
+        SpringApplication.run(DataServiceModule.class, args);
+    }
+}
