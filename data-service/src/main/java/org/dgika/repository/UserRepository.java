5d10deafb31ea428caa471fa6fb6ea3afbfe5b4d
Ika <rexar.ika@gmail.com>
2025-12-17 23:20:43 +0300

    added repository, first service, db testes, db ok

---
+++ data-service/src/main/java/org/dgika/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package org.dgika.repository;
+
+import org.dgika.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+}
---
+++ data-service/src/main/java/org/dgika/service/UserService.java
@@ -0,0 +1,19 @@
+package org.dgika.service;
+
+import org.dgika.model.User;
+import org.dgika.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User addUser(User user) {
+        return userRepository.save(user);
+    }
+}
